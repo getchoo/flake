4bb04d98461add1df39db556f5dd5de4426e6a97
seth <getchoo@tuta.io>
2023-01-29 01:07:40 -0500

    always enable noXlibs

--- hosts/common/desktop/default.nix
+++ hosts/common/desktop/default.nix
@@ -3,16 +3,15 @@
 	lib,
 	...
 }: let
-	value = config.sys.gui.enable;
-	reverse = !config.sys.gui.enable;
+	gui = config.sys.gui.enable;
 in {
 	imports = [
 		./gnome.nix
 		./plasma.nix
 	];
 
-	environment.noXlibs = lib.mkForce reverse;
-	programs.xwayland.enable = value;
-	services.xserver.enable = value;
-	xdg.portal.enable = value;
+	environment.noXlibs = lib.mkForce false;
+	programs.xwayland.enable = gui;
+	services.xserver.enable = gui;
+	xdg.portal.enable = gui;
 }
