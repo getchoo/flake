9c4a04054f5c72c3c66973b0bea06d10e042947c
seth <getchoo@tuta.io>
2023-02-20 16:45:26 -0500

    add catppuccin theme

--- users/seth/shell/fish.nix
+++ users/seth/shell/fish.nix
@@ -1,6 +1,22 @@
 { pkgs, ... }: {
+
+  xdg.configFile."fish/themes" = {
+    recursive = true;
+    source =
+      pkgs.fetchFromGitHub
+        {
+          owner = "catppuccin";
+          repo = "fish";
+          rev = "b90966686068b5ebc9f80e5b90fdf8c02ee7a0ba";
+          sha256 = "sha256-wQlYQyqklU/79K2OXRZXg5LvuIugK7vhHgpahpLFaOw=";
+        } + "/themes";
+  };
+
   programs.fish = {
     enable = true;
+    interactiveShellInit = ''
+      			fish_config theme choose "Catppuccin Mocha"
+      		'';
     plugins = [
       {
         name = "autopair-fish";
