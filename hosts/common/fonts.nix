11a22f6bf60fc1adf7d797d1ffbda573c4718239
seth <getchoo@tuta.io>
2023-03-05 09:39:31 -0500

    simplify fonts config

--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -2,32 +2,26 @@
   pkgs,
   desktop,
   ...
-}: let
-  gui = desktop != "";
-in {
+}:
+if desktop != ""
+then {
   fonts = {
-    enableDefaultFonts = gui;
-    fonts =
-      if gui
-      then
-        with pkgs; [
-          corefonts
-          fira-code
-          (nerdfonts.override {fonts = ["FiraCode"];})
-          noto-fonts
-          noto-fonts-extra
-          noto-fonts-emoji
-          noto-fonts-cjk-sans
-        ]
-      else [];
-    fontconfig.defaultFonts =
-      if gui
-      then {
-        serif = ["Noto Serif"];
-        sansSerif = ["Noto Sans"];
-        emoji = ["Noto Color Emoji"];
-        monospace = ["Fira Code"];
-      }
-      else {};
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      corefonts
+      fira-code
+      (nerdfonts.override {fonts = ["FiraCode"];})
+      noto-fonts
+      noto-fonts-extra
+      noto-fonts-emoji
+      noto-fonts-cjk-sans
+    ];
+    fontconfig.defaultFonts = {
+      serif = ["Noto Serif"];
+      sansSerif = ["Noto Sans"];
+      emoji = ["Noto Color Emoji"];
+      monospace = ["Fira Code"];
+    };
   };
 }
+else {}
