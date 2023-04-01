e9d5caa6b27728ba91b3fd62c36932d0fe251a42
seth <getchoo@tuta.io>
2023-04-01 12:16:14 -0400

    add nix-community binary cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "getchoo's flake for system configurations";
 
+  nixConfig = {
+    extra-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  };
+
   inputs = {
     nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
     agenix = {
--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -15,6 +15,12 @@
       auto-optimise-store = true;
       warn-dirty = false;
       experimental-features = ["nix-command" "flakes"];
+      trusted-substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 }
