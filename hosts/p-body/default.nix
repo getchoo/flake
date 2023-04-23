b4e31ae959e612771f411bd4f9ee05ad16ac8d19
seth <getchoo@tuta.io>
2023-04-23 06:58:49 -0400

    fixup networking for p-body

--- hosts/p-body/default.nix
+++ hosts/p-body/default.nix
@@ -14,6 +14,8 @@
     defaultPackages.enable = false;
   };
 
+  nixos.networking.enable = false;
+
   networking = {
     hostName = "p-body";
     firewall = let
@@ -34,69 +36,76 @@
   };
 
   services = {
-    caddy = {
-      enable = true;
+    #caddy = {
+    #  enable = true;
 
-      email = "getchoo@tuta.io";
+    #  email = "getchoo@tuta.io";
 
-      logFormat = ''
-        output stdout
-        format json
-      '';
+    #  logFormat = ''
+    #    output stdout
+    #    format json
+    #  '';
 
-      extraConfig = ''
-        (strip-www) {
-        	redir https://{args.0}{uri}
-        }
+    #  extraConfig = ''
+    #    (strip-www) {
+    #    	redir https://{args.0}{uri}
+    #    }
 
-        (common_domain) {
-        	encode gzip
+    #    (common_domain) {
+    #    	encode gzip
 
-        	handle {
-        		try_files {path} {path}/
-        	}
+    #    	handle {
+    #    		try_files {path} {path}/
+    #    	}
 
-        	handle_errors {
-        		@404 {
-        			expression {http.error.status_code} == 404
-        		}
-        		rewrite @404 /404.html
-        		file_server
-        	}
-        }
+    #    	handle_errors {
+    #    		@404 {
+    #    			expression {http.error.status_code} == 404
+    #    		}
+    #    		rewrite @404 /404.html
+    #    		file_server
+    #    	}
+    #    }
 
-        (no_embeds) {
-        	header /{args.0} X-Frame-Options DENY
-        }
+    #    (no_embeds) {
+    #    	header /{args.0} X-Frame-Options DENY
+    #    }
 
-        (container_proxy) {
-        	handle_path /{args.0}/* {
-        		reverse_proxy {args.1}
-        	}
-        }
-      '';
+    #    (container_proxy) {
+    #    	handle_path /{args.0}/* {
+    #    		reverse_proxy {args.1}
+    #    	}
+    #    }
+    #  '';
 
-      globalConfig = ''
-        auto_https off
-      '';
+    #  globalConfig = ''
+    #    auto_https off
+    #  '';
 
-      virtualHosts = {
-        guzzle = rec {
-          hostName = "198.199.68.30";
-          serverAliases = [
-            "www.${hostName}"
-          ];
-          extraConfig = ''
-            root * /var/www
-            import common_domain
+    #  virtualHosts = {
+    #    guzzle = rec {
+    #      hostName = "167.99.145.73";
 
-            file_server
+    #      serverAliases = [
+    #        "www.${hostName}"
+    #      ];
 
-            import container_proxy api :8000
-          '';
-        };
-      };
-    };
+    #      extraConfig = ''
+    #        root * /var/www
+    #        import common_domain
+
+    #        file_server
+
+    #        import container_proxy api :8000
+    #      '';
+
+    #      listenAddresses = [
+    #        "127.0.0.1"
+    #        "::1"
+    #      ];
+    #    };
+    #  };
+    #};
 
     endlessh = {
       enable = true;
@@ -106,8 +115,8 @@
 
     guzzle-api = {
       enable = true;
-      url = "http://198.199.68.30/api/api";
-      port = "8000";
+      url = "http://167.99.145.73";
+      port = "80";
       package = guzzle-api-server;
     };
 
