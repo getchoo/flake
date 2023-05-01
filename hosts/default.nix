d838bd39bc464fe4d9ec3260921e11d03f8328a7
seth <getchoo@tuta.io>
2023-04-30 22:02:09 -0400

    globally enable more substituters

--- hosts/default.nix
+++ hosts/default.nix
@@ -27,7 +27,25 @@ with inputs; let
           config.allowUnfree = true;
         };
 
-        nix.registry.getchoo.flake = getchoo;
+        nix = {
+          registry.getchoo.flake = getchoo;
+          settings = {
+            trusted-substituters = [
+              "https://getchoo.cachix.org"
+              "https://nix-community.cachix.org"
+              "https://hercules-ci.cachix.org"
+              "https://wurzelpfropf.cachix.org"
+            ];
+
+            trusted-public-keys = [
+              "getchoo.cachix.org-1:ftdbAUJVNaFonM0obRGgR5+nUmdLMM+AOvDOSx0z5tE="
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
+              "wurzelpfropf.cachix.org-1:ilZwK5a6wJqVr7Fyrzp4blIEkGK+LJT0QrpWr1qBNq0="
+            ];
+          };
+        };
+
         nixos.enable = true;
         system.stateVersion = "23.05";
       }
