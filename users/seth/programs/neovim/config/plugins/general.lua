8bb749cf6ad30f943d8cafb6e770d867316cdc47
seth <getchoo@tuta.io>
2023-05-04 17:22:39 -0400

    neovim: update bufferline config

--- users/seth/programs/neovim/config/plugins/general.lua
+++ users/seth/programs/neovim/config/plugins/general.lua
@@ -32,9 +32,12 @@ vim.api.nvim_command("colorscheme catppuccin")
 ---- bufferline
 require("bufferline").setup({
 	options = {
+		always_show_bufferline = false,
 		highlights = require("catppuccin.groups.integrations.bufferline").get(),
 		diagnostics = "nvim_lsp",
+		mode = "buffers",
 		numbers = "ordinal",
+		separator_style = "slant",
 	},
 })
 
