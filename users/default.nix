589c80eb1bcaf76b4a1a7e6f9cdd725fdb04e063
seth <getchoo@tuta.io>
2023-05-07 23:35:26 -0400

    users: improve mkHMUser

--- users/default.nix
+++ users/default.nix
@@ -1,29 +1,31 @@
 {inputs, ...}: let
-  inherit (inputs) getchoo home-manager nixpkgsUnstable nur;
+  mkHMUser = {
+    name,
+    modules ? [],
+    pkgs ? import inputs.nixpkgs {system = "x86_64-linux";},
+    extraSpecialArgs ? inputs,
+  }:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs extraSpecialArgs;
+      modules =
+        [
+          {
+            programs.home-manager.enable = true;
+          }
+        ]
+        ++ [./${name}/home.nix]
+        ++ modules;
+    };
 in {
   perSystem = {system, ...}: {
-    legacyPackages.homeConfigurations = let
-      inherit (home-manager.lib) homeManagerConfiguration;
-      modules = [
-        {
-          programs.home-manager.enable = true;
-        }
-      ];
-
-      extraSpecialArgs = inputs;
-
-      pkgs = import nixpkgsUnstable {
-        inherit system;
-        overlays = [nur.overlay getchoo.overlays.default];
-      };
-
-      mkHMUser = username:
-        homeManagerConfiguration {
-          inherit pkgs extraSpecialArgs;
-          modules = modules ++ ["./${username}"];
+    legacyPackages.homeConfigurations = {
+      seth = mkHMUser {
+        name = "seth";
+        pkgs = import inputs.nixpkgsUnstable {
+          inherit system;
+          overlays = with inputs; [nur.overlay getchoo.overlays.default];
         };
-    in {
-      seth = mkHMUser "seth";
+      };
     };
   };
 }
