029b07db8737aa7f0ca921c6993ff1e09dfe93eb
seth <getchoo@tuta.io>
2023-05-19 12:08:05 -0400

    p-body: make grafana public

--- hosts/p-body/grafana.nix
+++ hosts/p-body/grafana.nix
@@ -4,10 +4,21 @@ in {
   services.grafana = {
     enable = true;
     settings = {
+      "auth.anonymous" = {
+        enabled = true;
+        hide_version = true;
+        org_name = "getchoosystems";
+        org_role = "Viewer";
+      };
+
       server = {
         http_addr = "127.0.0.1";
         http_port = 4000;
         domain = "grafana.${domain}";
+      };
+
+      feature_toggles = {
+        publicDashboards = true;
       };
     };
   };
