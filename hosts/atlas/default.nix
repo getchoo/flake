f0ed286c00c8369a9c99986c7574b60954120d94
seth <getchoo@tuta.io>
2023-07-15 03:00:28 -0400

    host/atlas+p-body: stop using openssh

--- hosts/atlas/default.nix
+++ hosts/atlas/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }: {
@@ -33,35 +32,21 @@
     hostName = "atlas";
   };
 
-  nix.settings = {
-    extra-platforms = lib.mkForce "";
-    trusted-users = ["bob"];
-  };
+  nix.settings.allowed-users = ["bob"];
 
-  system.stateVersion = "22.11";
+  system.stateVersion = "23.05";
 
-  users.users = let
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMPV9wiDwXVyoVh347CAulkdGzG7+1m/rZ1aV5fk3BHM atlas getchoo@tuta.io"
-    ];
-  in {
-    root = {inherit openssh;};
-
+  users.users = {
     atlas = {
       extraGroups = ["wheel"];
       isNormalUser = true;
       shell = pkgs.bash;
       passwordFile = config.age.secrets.userPassword.path;
-      inherit openssh;
     };
 
     bob = {
       isNormalUser = true;
       shell = pkgs.bash;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOtbxHjDADxqsG+AgCoiDq0uCsgcnJCIH+9rB6K5pIi9 p-body@p-body"
-        "ssh-ed25519 aaaac3nzac1lzdi1nte5aaaaimpv9widwxvyovh347caulkdgzg7+1m/rz1av5fk3bhm atlas getchoo@tuta.io"
-      ];
     };
   };
 
