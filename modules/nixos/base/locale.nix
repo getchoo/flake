dc74f7e340309f484a028a27d4768de45f3106aa
seth <getchoo@tuta.io>
2023-07-14 21:59:17 -0400

    modules/base: don't use hardware clock

--- modules/nixos/base/locale.nix
+++ modules/nixos/base/locale.nix
@@ -16,9 +16,6 @@ in {
       defaultLocale = "en_US.UTF-8";
     };
 
-    time = {
-      hardwareClockInLocalTime = true;
-      timeZone = "America/New_York";
-    };
+    time.timeZone = "America/New_York";
   };
 }
