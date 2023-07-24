3ecd5b770bc5cb1f3ba02bd627f4679a14a6a6e6
seth <getchoo@tuta.io>
2023-07-23 22:30:05 -0400

    parts/overlay: make overlay compatible with darwin

--- parts/overlays/fish.nix
+++ parts/overlays/fish.nix
@@ -1,10 +1,14 @@
 _: prev: {
-  fish = prev.symlinkJoin {
-    inherit (prev.fish) passthru;
-    name = "fish-nodesktop";
-    paths = [prev.fish];
-    postBuild = ''
-      rm $out/share/applications/fish.desktop
-    '';
-  };
+  fish =
+    if prev.stdenv.isLinux
+    then
+      prev.symlinkJoin {
+        inherit (prev.fish) passthru;
+        name = "fish-nodesktop";
+        paths = [prev.fish];
+        postBuild = ''
+          rm $out/share/applications/fish.desktop
+        '';
+      }
+    else prev.fish;
 }
--- parts/overlays/neovim.nix
+++ parts/overlays/neovim.nix
@@ -1,10 +1,14 @@
 _: prev: {
-  neovim = prev.symlinkJoin {
-    inherit (prev.neovim) passthru;
-    name = "neovim-nodesktop";
-    paths = [prev.neovim];
-    postBuild = ''
-      rm $out/share/applications/*.desktop
-    '';
-  };
+  neovim =
+    if prev.stdenv.isLinux
+    then
+      prev.symlinkJoin {
+        inherit (prev.neovim) passthru;
+        name = "neovim-nodesktop";
+        paths = [prev.neovim];
+        postBuild = ''
+          rm $out/share/applications/*.desktop
+        '';
+      }
+    else prev.neovim;
 }
