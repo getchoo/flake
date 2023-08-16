ce0784d64fd7775bbdf7c0bb0f9b6cb8a4682715
seth <getchoo@tuta.io>
2023-08-16 14:42:06 -0400

    users/seth: add hw accel flags to chromium

--- users/seth/desktop/default.nix
+++ users/seth/desktop/default.nix
@@ -17,14 +17,29 @@ in {
 
   options.desktop.enable = mkEnableOption "enable desktop configuration";
 
-  config.home = mkIf cfg.enable {
-    packages = with pkgs; [
-      chromium
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
       discord
       element-desktop
       spotify
       steam
       prismlauncher
     ];
+
+    programs = {
+      chromium = {
+        enable = true;
+        # hw accel support
+        commandLineArgs = [
+          "--ignore-gpu-blocklist"
+          "--enable-gpu-rasterization"
+          "--enable-gpu-compositing"
+          "--enable-native-gpu-memory-buffers"
+          "--enable-zero-copy"
+          "--enable-features=VaapiVideoDecoder,VaapiVideoEncoder,CanvasOopRasterization,RawDraw,WebRTCPipeWireCapturer,Vulkan,WaylandWindowDecorations,WebUIDarkMode"
+          "--force-dark-mode"
+        ];
+      };
+    };
   };
 }
