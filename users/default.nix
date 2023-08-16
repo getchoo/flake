c1d148598383944a78aec0183e27f253e370c3df
seth <getchoo@tuta.io>
2023-08-16 10:46:16 -0400

    hosts+users: use new lib to generate configs

--- users/default.nix
+++ users/default.nix
@@ -1,26 +1,19 @@
 {
   inputs,
-  myLib,
   self,
   ...
 }: {
-  perSystem = {system, ...}: let
-    inherit (myLib.configs inputs) mkHMUsers;
-    ci = myLib.ci [system];
-  in {
-    homeConfigurations = mkHMUsers {
+  flake.homeConfigurations = let
+    users = {
       seth = {
-        pkgs = import inputs.nixpkgs {
-          inherit system;
+        nixpkgsArgs = {
           overlays = with inputs; [nur.overlay getchoo.overlays.default];
         };
         modules = [
-          {_module.args.osConfig = {};}
           inputs.nix-index-database.hmModules.nix-index
         ];
       };
     };
-
-    packages = (ci.mkCompatibleHM self.homeConfigurations).${system};
-  };
+  in
+    self.lib.configs.genHMCfgs users;
 }
