ab34482928fc8af7e73f6600db716ec6d9d29bff
seth <getchoo@tuta.io>
2023-08-17 14:23:10 -0400

    modules/server/acme: fix email

--- modules/nixos/server/acme.nix
+++ modules/nixos/server/acme.nix
@@ -17,7 +17,7 @@ in {
     security.acme = {
       acceptTerms = true;
       defaults = {
-        email = "tuta.io";
+        email = "getchoo@tuta.io";
         dnsProvider = "cloudflare";
         credentialsFile = config.age.secrets.cloudflareApiKey.path;
       };
