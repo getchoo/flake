2b488b9facc84c862ec53efc0bdd60881275b6d1
seth <getchoo@tuta.io>
2023-08-19 13:28:24 -0400

    parts/ci: fix system declaration

--- parts/ci.nix
+++ parts/ci.nix
@@ -21,10 +21,10 @@ in {
       inherit (builtins) elem;
       inherit (lib) filterAttrs mapAttrs mkForce;
 
-      amd64 = ["x86-64-linux"];
+      amd64 = ["x86_64-linux"];
 
-      findCompatible = system: filterAttrs (s: _: elem s system);
-      findCompatibleCfgs = system: filterAttrs (_: v: elem v.pkgs.system system);
+      findCompatible = systems: filterAttrs (s: _: elem s systems);
+      findCompatibleCfgs = systems: filterAttrs (_: v: elem v.pkgs.system systems);
       buildCfgs = mapAttrs (_: v: v.config.system.build.toplevel);
       buildHMUsers = mapAttrs (_: mapAttrs (_: v: v.activationPackage));
       #evalCfgs = mapAttrs (_: v: seq v.config.system.build.toplevel v.pkgs.emptyFile);
@@ -34,7 +34,7 @@ in {
           checks = findCompatible amd64 self.checks;
           devShells = findCompatible amd64 self.devShells;
           homeConfigurations = buildHMUsers (findCompatible amd64 self.homeConfigurations);
-          nixosConfigurations = buildCfgs (findCompatibleCfgs self.nixosConfigurations);
+          nixosConfigurations = buildCfgs (findCompatibleCfgs ciSystems self.nixosConfigurations);
         };
       };
 
