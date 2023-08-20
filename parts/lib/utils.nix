5b90e3fa504258121026ea24287ef5f02a34cd31
seth <getchoo@tuta.io>
2023-08-19 20:05:23 -0400

    lib/mkDeployNodes: use pkgs from system

--- parts/lib/utils.nix
+++ parts/lib/utils.nix
@@ -1,36 +1,30 @@
 {
   self,
   inputs,
-  withSystem,
   ...
 }: let
-  deployPkgs = import inputs.nixpkgs rec {
-    system = "x86_64-linux";
-    overlays = [
-      inputs.deploy-rs.overlay
-      (_: prev: {
-        deploy-rs = {
-          inherit (withSystem system (p: p.pkgs)) deploy-rs;
-          inherit (prev.deploy-rs) lib;
-        };
-      })
-    ];
-  };
+  deployPkgs = pkgs:
+    import pkgs.path {
+      inherit (pkgs) system;
+      overlays = [
+        inputs.deploy-rs.overlay
+        (_: prev: {
+          deploy-rs = {
+            inherit (pkgs) deploy-rs;
+            inherit (prev.deploy-rs) lib;
+          };
+        })
+      ];
+    };
 in {
-  mkDeployNodes = hosts: let
-    inherit (builtins) attrNames listToAttrs map;
-    vals =
-      map (name: let
-        system = self.nixosConfigurations.${name};
-      in {
-        inherit name;
-        value = {
-          sshUser = "root";
-          hostname = system.config.networking.hostName;
-          profiles.system.path = deployPkgs.deploy-rs.lib.activate.nixos system;
-        };
-      })
-      (attrNames hosts);
-  in
-    listToAttrs vals;
+  mkDeployNodes = hosts:
+    inputs.nixpkgs.lib.genAttrs hosts (host: let
+      system = self.nixosConfigurations.${host};
+      inherit (system) pkgs;
+      inherit (deployPkgs pkgs) deploy-rs;
+    in {
+      sshUser = "root";
+      hostname = system.config.networking.hostName;
+      profiles.system.path = deploy-rs.lib.activate.nixos system;
+    });
 }
