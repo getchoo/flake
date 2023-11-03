bd21bc5470f4d47aff38788b6df90c48191890b0
seth <getchoo@tuta.io>
2023-11-03 18:50:49 -0400

    seth/starship: merge catppuccin palette with personal config

--- users/seth/programs/starship/default.nix
+++ users/seth/programs/starship/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   programs.starship = {
     enable = true;
     enableBashIntegration = false;
@@ -9,14 +9,6 @@
         palette = "catppuccin_mocha";
         command_timeout = 250;
       }
-      // fromTOML (builtins.readFile ./starship.toml)
-      // fromTOML (builtins.readFile
-        (pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "starship";
-            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f";
-            hash = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
-          }
-          + "/palettes/mocha.toml"));
+      // fromTOML (builtins.readFile ./starship.toml);
   };
 }
