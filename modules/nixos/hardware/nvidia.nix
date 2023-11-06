714634ec755cd97fb5bd95845899e10defbed09c
seth <getchoo@tuta.io>
2023-11-06 14:26:32 -0500

    modules/nvidia: don't use vaapiVdpau

--- modules/nixos/hardware/nvidia.nix
+++ modules/nixos/hardware/nvidia.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: let
   cfg = config.hardware.nvidia;
@@ -10,20 +9,9 @@ in {
   options.hardware.nvidia.enable = mkEnableOption "enable nvidia support";
 
   config = mkIf cfg.enable {
-    environment.sessionVariables = {
-      LIBVA_DRIVER_NAME = "vdpau";
-      VDPAU_DRIVER = "nvidia";
-    };
-
-    hardware = {
-      nvidia = {
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
-        modesetting.enable = true;
-      };
-
-      opengl = {
-        extraPackages = [(lib.setPrio (-11) pkgs.vaapiVdpau)];
-      };
+    hardware.nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
     };
 
     services.xserver.videoDrivers = ["nvidia"];
