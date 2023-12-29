3eb380133c19b3a50213d6ec416a1fa9c3eab52a
seth <getchoo@tuta.io>
2023-12-29 06:46:46 -0500

    devshell: remove jq & nix-eval-jobs

--- dev/shell.nix
+++ dev/shell.nix
@@ -23,8 +23,6 @@
             deploy-rs
             fzf
             just
-            jq
-            nix-eval-jobs
             opentofu' # see ../terranix/
           ]
           ++ lib.optional stdenv.isLinux inputs'.agenix.packages.agenix;
