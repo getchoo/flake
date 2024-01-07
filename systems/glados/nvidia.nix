ca7d0c1bc109f9931faabc1d0672ca604f3f335a
seth <getchoo@tuta.io>
2024-01-07 18:15:16 -0500

    glados: use prod driver for nvidia

--- systems/glados/nvidia.nix
+++ systems/glados/nvidia.nix
@@ -1,10 +1,16 @@
-{inputs, ...}: {
+{
+  config,
+  inputs,
+  ...
+}: {
   imports = with inputs; [
     nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
     nixos-hardware.nixosModules.common-pc-ssd
   ];
 
   hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+
     modesetting.enable = true;
   };
 }
