3ac4556e7cc8ebf3d9015f2edf5ce0ed4e5f5d05
seth <getchoo@tuta.io>
2024-01-09 01:54:34 -0500

    glados: pin nvidia driver to 535.146.02

--- systems/glados/nvidia.nix
+++ systems/glados/nvidia.nix
@@ -9,7 +9,16 @@
   ];
 
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    # 545 isn't stable enough yet
+    # production driver as of 6a9382b8d1d9531be1a2fd611f3f91f30fb38678
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "535.146.02";
+      sha256_64bit = "sha256-Sf0cyeRFyYspP3xm82vs/hLMwd6WDf/z8dyWujqcv3A=";
+      sha256_aarch64 = "sha256-8G0oNdaVWxIGwVaQSw/cojy4TIAuiUBF3B98BI4hEec=";
+      openSha256 = "sha256-Oyllcy3uYYK912CIusMwjKKHtMgoyOxpZWQQ8hIycuk=";
+      settingsSha256 = "sha256-IrN2NaPrZSN0sCZqYNJ43iCicX3ziwUgyLLSRzp9sHQ=";
+      persistencedSha256 = "sha256-trIddaTgKXszEJunK+t6D+e3HbLDTfAsitdEYRgwRNQ=";
+    };
 
     modesetting.enable = true;
   };
