e5c54d409259c6f651b0616771f91b7a26070cac
seth <getchoo@tuta.io>
2024-02-08 21:06:05 -0500

    traits: consistently describe manageSecrets options

--- modules/nixos/traits/hercules.nix
+++ modules/nixos/traits/hercules.nix
@@ -9,39 +9,43 @@
 in {
   options.traits.hercules-ci = {
     enable = lib.mkEnableOption "hercules-ci";
-    manageSecrets = lib.mkEnableOption "automatic secrets management";
+    manageSecrets =
+      lib.mkEnableOption "automatic secrets management"
+      // {
+        default = config.traits.secrets.enable;
+      };
   };
 
   config = lib.mkIf cfg.enable (
     lib.mkMerge [
       {
-        services = {
-          hercules-ci-agent = {
-            enable = true;
-            package = unstable.hercules-ci-agent;
-            settings = {
-              binaryCachesPath = config.age.secrets.binaryCache.path;
-              clusterJoinTokenPath = config.age.secrets.clusterToken.path;
-              secretsJsonPath = config.age.secrets.secretsJson.path;
-            };
-          };
+        services.hercules-ci-agent = {
+          enable = true;
+          package = unstable.hercules-ci-agent;
         };
       }
 
       (let
-        hercArgs = {
-          mode = "400";
-          owner = "hercules-ci-agent";
-          group = "hercules-ci-agent";
-        };
-
-        mkSecrets = lib.mapAttrs (_: file: lib.recursiveUpdate hercArgs {inherit file;});
+        secretNames = [
+          "binaryCaches"
+          "clusterJoinToken"
+          "secretsJson"
+        ];
       in
         lib.mkIf cfg.manageSecrets {
-          age.secrets = mkSecrets {
-            binaryCache = secretsDir + "/binaryCache.age";
-            clusterToken = secretsDir + "/clusterToken.age";
-            secretsJson = secretsDir + "/secretsJson.age";
+          age.secrets = lib.genAttrs secretNames (
+            file: {
+              file = "${secretsDir}/${file}.age";
+              mode = "400";
+              owner = "hercules-ci-agent";
+              group = "hercules-ci-agent";
+            }
+          );
+
+          services.hercules-ci-agent = {
+            settings = lib.mapAttrs' (name: lib.nameValuePair (name + "Path")) (
+              lib.genAttrs secretNames (name: config.age.secrets.${name}.path)
+            );
           };
         })
     ]
