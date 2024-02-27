82e202287f4f25afe411d93d2f437f0977c52570
seth <getchoo@tuta.io>
2024-02-27 10:35:28 -0500

    nixos/containers: use new option for nvidia-container-toolkit when possible

--- modules/nixos/traits/containers.nix
+++ modules/nixos/traits/containers.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  options,
   pkgs,
   ...
 }: let
@@ -10,14 +11,28 @@ in {
     enable = lib.mkEnableOption "containers support";
   };
 
-  config.virtualisation = lib.mkIf cfg.enable {
-    podman = {
-      enable = true;
-      enableNvidia = lib.mkDefault (builtins.elem "nvidia" (config.services.xserver.videoDrivers or []));
-      extraPackages = with pkgs; [podman-compose];
-      autoPrune.enable = true;
-    };
+  config.virtualisation = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      podman = {
+        enable = true;
+        extraPackages = with pkgs; [podman-compose];
+        autoPrune.enable = true;
+      };
 
-    oci-containers.backend = "podman";
-  };
+      oci-containers.backend = "podman";
+    })
+
+    (let
+      enable = lib.mkDefault (
+        lib.elem "nvidia" (config.services.xserver.videoDrivers or [])
+      );
+    in
+      if (options.virtualisation.containers ? cdi)
+      then {
+        containers.cdi.dynamic.nvidia = {inherit enable;};
+      }
+      else {
+        podman.enableNvidia = enable;
+      })
+  ];
 }
