485c482441e268c7890f947e5d4f0d6e64d8d522
seth <getchoo@tuta.io>
2024-03-07 14:32:12 -0500

    nixos/plasma: don't use plasma 6 on < 24.05

--- modules/nixos/desktop/plasma/default.nix
+++ modules/nixos/desktop/plasma/default.nix
@@ -8,25 +8,30 @@
 in {
   options.desktop.plasma.enable = lib.mkEnableOption "Plasma desktop";
 
-  config = lib.mkIf cfg.enable {
-    environment = {
-      plasma6.excludePackages = with pkgs.libsForQt5; [
-        khelpcenter
-        plasma-browser-integration
-        print-manager
-      ];
-    };
-
-    services.xserver = {
-      displayManager.sddm = {
-        enable = true;
-        wayland.enable = true;
+  config = lib.mkIf cfg.enable (
+    # this is bad, i don't care
+    if (lib.versionAtLeast lib.version "24.05pre-git")
+    then {
+      environment = {
+        plasma6.excludePackages = with pkgs.libsForQt5; [
+          khelpcenter
+          plasma-browser-integration
+          print-manager
+        ];
       };
 
-      desktopManager.plasma6 = {
-        enable = true;
-        enableQt5Integration = true;
+      services.xserver = {
+        displayManager.sddm = {
+          enable = true;
+          wayland.enable = true;
+        };
+
+        desktopManager.plasma6 = {
+          enable = true;
+          enableQt5Integration = true;
+        };
       };
-    };
-  };
+    }
+    else {}
+  );
 }
