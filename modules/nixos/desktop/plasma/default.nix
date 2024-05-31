7168a84dc02191dd498ac1fbadb8da945d41477b
seth <getchoo@tuta.io>
2024-05-31 06:55:51 -0400

    modules: support >= 24.05

--- modules/nixos/desktop/plasma/default.nix
+++ modules/nixos/desktop/plasma/default.nix
@@ -9,34 +9,31 @@
 in {
   options.desktop.plasma.enable = lib.mkEnableOption "Plasma desktop";
 
-  config = lib.mkIf cfg.enable (
-    # this is bad, i don't care
-    lib.optionalAttrs (lib.versionAtLeast lib.version "24.05pre-git") {
-      environment = {
-        plasma6.excludePackages = with pkgs.kdePackages; [
-          khelpcenter
-          plasma-browser-integration
-          print-manager
-        ];
+  config = lib.mkIf cfg.enable {
+    environment = {
+      plasma6.excludePackages = with pkgs.kdePackages; [
+        khelpcenter
+        plasma-browser-integration
+        print-manager
+      ];
 
-        sessionVariables = {
-          NIXOS_OZONE_WL = "1";
-        };
-
-        systemPackages = [
-          pkgs.haruna
-          inputs.krunner-nix.packages.${pkgs.system}.default
-        ];
+      sessionVariables = {
+        NIXOS_OZONE_WL = "1";
       };
 
-      services.xserver = {
-        displayManager.sddm = {
-          enable = true;
-          wayland.enable = true;
-        };
+      systemPackages = [
+        pkgs.haruna
+        inputs.krunner-nix.packages.${pkgs.system}.default
+      ];
+    };
 
-        desktopManager.plasma6.enable = true;
+    services = {
+      displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
       };
-    }
-  );
+
+      desktopManager.plasma6.enable = true;
+    };
+  };
 }
