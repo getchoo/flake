d03d8a8c316c142477d0b96a007dd69559f0498c
seth <getchoo@tuta.io>
2024-05-31 06:49:24 -0400

    flake: nixpkgs-stable 23.11 -> 24.05

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "nixpkgs/nixos-24.05";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
