7ea38862eb5e17bdb28078e4dbc9a0c5bdd52e6f
seth <getchoo@tuta.io>
2024-06-30 10:07:34 -0400

    flake: drop flake-checks

--- checks.nix
+++ checks.nix
@@ -1,23 +1,42 @@
-{ inputs, ... }:
 {
   perSystem =
-    { lib, pkgs, ... }:
+    { pkgs, ... }:
     {
       checks = {
-        inherit
-          (inputs.flake-checks.lib.mkChecks {
-            inherit pkgs;
-            root = lib.fileset.toSource {
-              root = ./.;
-              fileset = lib.fileset.gitTracked ./.;
-            };
-          })
-          actionlint
-          alejandra
-          deadnix
-          editorconfig
-          statix
-          ;
+        check-formatting =
+          pkgs.runCommand "check-formatting" { nativeBuildInputs = [ pkgs.nixfmt-rfc-style ]; }
+            ''
+              cd ${./.}
+
+              echo "running nixfmt..."
+              nixfmt --check .
+
+              touch $out
+            '';
+
+        check-lint =
+          pkgs.runCommand "check-lint"
+            {
+              nativeBuildInputs = [
+                pkgs.actionlint
+                pkgs.deadnix
+                pkgs.statix
+              ];
+            }
+            ''
+              cd ${./.}
+
+              echo "running actionlint..."
+              actionlint ./.github/workflows/*
+
+              echo "running deadnix..."
+              deadnix --fail
+
+              echo "running statix..."
+              statix check .
+
+              touch $out
+            '';
       };
     };
 }
