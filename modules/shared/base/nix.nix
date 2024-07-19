4f51d72266d2e07322df78259b29d55b14c72911
seth <getchoo@tuta.io>
2024-07-19 02:23:06 -0400

    modules/nix: fix lix version comparison
    according to `lib.versionAtLeast` `2.90.0` >= `2.90.0-rc1`, but the
    opposite is true :/
    
    am i too tired to understand how this is actually right? or is this
    incorrect? who knows

--- modules/shared/base/nix.nix
+++ modules/shared/base/nix.nix
@@ -34,7 +34,7 @@ in
           # TODO: remove this nonsense when all implementations remove repl-flake
           ++ lib.optional (
             lib.versionOlder config.nix.package.version "2.22.0" # repl-flake was removed in nix 2.22.0
-            || lib.versionAtLeast config.nix.package.version "2.90.0-rc1" # but not in lix yet
+            || lib.versionAtLeast config.nix.package.version "2.90.0" # but not in lix yet
           ) "repl-flake";
 
         trusted-substituters = [ "https://getchoo.cachix.org" ];
