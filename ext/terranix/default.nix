c20ed35827d4abed986c3a8340cabfd2aad751f3
seth <getchoo@tuta.io>
2024-10-08 11:09:41 -0400

    terranix: add apply script

--- ext/terranix/default.nix
+++ ext/terranix/default.nix
@@ -1,24 +1,44 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, system, ... }:
     {
-      packages = {
-        opentofu = pkgs.opentofu.withPlugins (plugins: [
-          plugins.cloudflare
-          plugins.tailscale
-        ]);
+      lib,
+      pkgs,
+      self',
+      system,
+      ...
+    }:
+    let
+      inherit (self'.packages) opentofu;
 
-        terranix = inputs.terranix.lib.terranixConfiguration {
-          inherit system;
-          modules = [
-            ./cloudflare
-            ./tailscale
-            ./cloud.nix
-            ./vars.nix
-            ./versions.nix
-          ];
+      terranix = inputs.terranix.lib.terranixConfiguration {
+        inherit system;
+        modules = [
+          ./cloudflare
+          ./tailscale
+          ./cloud.nix
+          ./vars.nix
+          ./versions.nix
+        ];
+      };
+    in
+    {
+      apps = {
+        apply-tf = {
+          type = "app";
+          program = lib.getExe (
+            pkgs.writeShellScriptBin "apply" ''
+              cp --force ${terranix} config.tf.json \
+                && ${lib.getExe opentofu} init \
+                && ${lib.getExe opentofu} apply
+            ''
+          );
         };
       };
+
+      packages.opentofu = pkgs.opentofu.withPlugins (plugins: [
+        plugins.cloudflare
+        plugins.tailscale
+      ]);
     };
 }
