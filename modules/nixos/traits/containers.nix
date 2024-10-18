fbbb6f680e8d2b12e8f72ca6701b1550729123f7
seth <getchoo@tuta.io>
2024-10-18 16:56:19 -0400

    nixos/containers: set `hardware.nvidia-container-toolkit` in nvidia module

--- modules/nixos/traits/containers.nix
+++ modules/nixos/traits/containers.nix
@@ -10,29 +10,17 @@ in
 {
   options.traits.containers = {
     enable = lib.mkEnableOption "support for containers";
-    enableNvidia = lib.mkEnableOption "the use of nvidia-container-toolkit" // {
-      default = lib.elem "nvidia" config.services.xserver.videoDrivers;
-      defaultText = lib.literalExpression ''
-        lib.elem "nvidia" config.services.xserver.videoDrivers
-      '';
-    };
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkMerge [
-      {
-        virtualisation = {
-          podman = {
-            enable = true;
-            extraPackages = [ pkgs.podman-compose ];
-            autoPrune.enable = true;
-          };
+  config = lib.mkIf cfg.enable {
+    virtualisation = {
+      podman = {
+        enable = true;
+        extraPackages = [ pkgs.podman-compose ];
+        autoPrune.enable = true;
+      };
 
-          oci-containers.backend = "podman";
-        };
-      }
-
-      (lib.mkIf cfg.enableNvidia { hardware.nvidia-container-toolkit.enable = true; })
-    ]
-  );
+      oci-containers.backend = "podman";
+    };
+  };
 }
