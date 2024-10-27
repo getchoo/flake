9415ea9c61f8f416c133e7c7b55e5e29a9815db0
seth <getchoo@tuta.io>
2024-10-27 02:39:07 -0400

    seth: cleanup `base` submodule

--- users/seth/default.nix
+++ users/seth/default.nix
@@ -1,9 +1,16 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.seth;
 in
 {
   options.seth = {
+    enable = lib.mkEnableOption "Seth's home configuration";
+
     shellAliases.enable = lib.mkEnableOption "shell aliases" // {
       default = config.seth.enable;
       defaultText = lib.literalExpression "config.seth.enable";
@@ -13,32 +20,50 @@ in
       default = config.seth.enable;
       defaultText = lib.literalExpression "config.seth.enable";
     };
+
+    standalone.enable = lib.mkEnableOption "standalone configuration mode";
   };
 
   imports = [
-    ./base
     ./desktop
     ./programs
     ./services
     ./tweaks
   ];
 
-  config = lib.mkMerge [
-    (lib.mkIf cfg.shellAliases.enable {
-      home.shellAliases = {
-        diff = "diff --color=auto";
-        g = "git";
-        gs = "g status";
-      };
-    })
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      { home.stateVersion = "23.11"; }
 
-    (lib.mkIf cfg.shellVariables.enable {
-      home.sessionVariables = {
-        EDITOR = "nvim";
-        VISUAL = config.home.sessionVariables.EDITOR;
-        CARGO_HOME = "${config.xdg.dataHome}/cargo";
-        LESSHISTFILE = "${config.xdg.stateHome}/less/history";
-      };
-    })
-  ];
+      (lib.mkIf cfg.shellAliases.enable {
+        home.shellAliases = {
+          diff = "diff --color=auto";
+          g = "git";
+          gs = "g status";
+        };
+      })
+
+      (lib.mkIf cfg.shellVariables.enable {
+        home.sessionVariables = {
+          EDITOR = "nvim";
+          VISUAL = config.home.sessionVariables.EDITOR;
+          CARGO_HOME = "${config.xdg.dataHome}/cargo";
+          LESSHISTFILE = "${config.xdg.stateHome}/less/history";
+        };
+      })
+
+      (lib.mkIf cfg.standalone.enable {
+        # This won't be set in standalone configurations
+        _module.args.osConfig = { };
+
+        # Make sure we can switch & update
+        programs.home-manager.enable = true;
+
+        home = {
+          username = "seth";
+          homeDirectory = (if pkgs.stdenv.isDarwin then "/Users" else "/home") + "/${config.home.username}";
+        };
+      })
+    ]
+  );
 }
