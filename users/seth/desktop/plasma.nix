c1c95b28255d63ab23606803465de288fa68ec46
seth <getchoo@tuta.io>
2025-01-05 04:54:07 -0500

    seth/plasma: update apps

--- users/seth/desktop/plasma.nix
+++ users/seth/desktop/plasma.nix
@@ -9,7 +9,14 @@ let
 in
 {
   config = lib.mkIf enable {
-    home.packages = with pkgs; [ papirus-icon-theme ];
+    home.packages = [
+      # Matrix client
+      pkgs.kdePackages.neochat
+      # Mastodon client
+      pkgs.kdePackages.tokodon
+      # Torrent client
+      pkgs.qbittorrent
+    ];
 
     xdg = {
       dataFile."konsole/catppuccin-mocha.colorscheme".source =
@@ -17,7 +24,7 @@ in
           owner = "catppuccin";
           repo = "konsole";
           rev = "7d86b8a1e56e58f6b5649cdaac543a573ac194ca";
-          sha256 = "EwSJMTxnaj2UlNJm1t6znnatfzgm1awIQQUF3VPfCTM=";
+          hash = "EwSJMTxnaj2UlNJm1t6znnatfzgm1awIQQUF3VPfCTM=";
         }
         + "/Catppuccin-Mocha.colorscheme";
     };
